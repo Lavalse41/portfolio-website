9a86a23ec1b2e46745a6d08fadbdae9a911952ba
unknown <ratiporn.pr@gmail.com>
2023-09-18 08:59:43 +0700

    fix: portfolio slide

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -5,36 +5,55 @@ function Project() {
   return (
     <section className="sec-3">
       <p className="head head-2">My Lastest Project</p>
-      <div class>
-        <div id="wrapper-outer-port">
-          {portfolio.map((item, index) => {
-            return (
-              <div key={index} id="wrapper-port">
-                <img
-                  src={item.img.src}
-                  alt={item.img.alt}
-                  height="350"
-                  width="350"
-                ></img>
-                <p className="text">{item.title}</p>
-              </div>
-            );
-          })}
+
+      <div className="slider">
+        <input type="radio" name="slider" id="slide1" checked></input>
+        <input type="radio" name="slider" id="slide2"></input>
+        <div id="bullets">
+          <label htmlFor="slide1"></label>
+          <label htmlFor="slide2"></label>
         </div>
-        <div id="wrapper-outer-port">
-          {portfolio.map((item, index) => {
-            return (
-              <div key={index} id="wrapper-port">
-                <img
-                  src={item.img.src}
-                  alt={item.img.alt}
-                  height="350"
-                  width="350"
-                ></img>
-                <p className="text">{item.title}</p>
+
+        <div className="slides">
+          <div className="overflow">
+            <div className="inner">
+              <div className="slide slide_1">
+                <div className="wrapper-port">
+                  {portfolio.map((item, index) => {
+                    return (
+                      <div key={index}>
+                        <img
+                          src={item.img.src}
+                          alt={item.img.alt}
+                          height="350"
+                          width="350"
+                        ></img>
+                        <p className="text">{item.title}</p>
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
-            );
-          })}
+
+              <div className="slide slide_2">
+                <div className="wrapper-port">
+                  {portfolio.map((item, index) => {
+                    return (
+                      <div key={index}>
+                        <img
+                          src={item.img.src}
+                          alt={item.img.alt}
+                          height="350"
+                          width="350"
+                        ></img>
+                        <p className="text">{item.title}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
